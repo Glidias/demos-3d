292ca30e7835676b50b24f2ee79bff38abcb3215
Glidias <glidias@hotmail.com>
2021-01-29 18:19:45 +0800

    aa

---
+++ hlod/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "639519.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 19934535, 14169775, 14169938, 14173407, 14175633, 20143426, 20153477, 20156088, 20156113, 20154240 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
